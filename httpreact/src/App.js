124710a3e49e93414b6445ccf32fedd34a97b763
Antonio Lindo <antoniolvitall@gmail.com>
2023-04-24 20:19:00 -0300

    adicao de dados com react

--- httpreact/src/App.js
+++ httpreact/src/App.js
@@ -6,6 +6,8 @@ import {useState, useEffect} from "react";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
   // 1-Resgatando dados
   useEffect(() => {
@@ -21,6 +23,23 @@ function App() {
     fetchData();
   }, []);
 
+  // 2- adicao de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const product = {
+      name,
+      price,
+
+    };
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product),
+    });
+  };
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
@@ -29,6 +48,19 @@ function App() {
           <li key={product.id}>{product.name} - R$: {product.price}</li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)}/>
+          </label>
+          <label>
+            Preco:
+            <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)}/>
+          </label>
+          <input type="submit" value="Criar"/>
+        </form>
+      </div>
     </div>
   );
 }
